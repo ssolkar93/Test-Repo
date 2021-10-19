99a69c958442a52d69f31cb5b62830a4a2534b9c
ssolkar93 <suhail.solkar@hslpathology.com>
2021-10-19 22:00:21 +0100

    Stage 2

--- Work please.py
+++ Work please.py
@@ -5,9 +5,7 @@ print (sys.version)
 print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+
 
 
 r = requests.get("http://google.com")
